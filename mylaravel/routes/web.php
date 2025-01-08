49f358d73adba2fa43ec2119132e854448b0a183
peemMichan <naprarin2682@gmail.com>
2025-01-09 00:54:55 +0700

    laravel

--- mylaravel/routes/web.php
+++ mylaravel/routes/web.php
@@ -1,11 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Mycontroller;
+
+Route :: get('/mycontroller/{var1?}',[MyController::class,'myfunction']);
+
+Route :: post('/mycontroller/{id?}',[MyController::class,'myfunction']);
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/hello', function () {
-    return "<h1>Hello world!</h1>";
+Route::get('/hello/{id}',function ($val="") {
+    return "<h1>Hello world! $val</h1>";
 });
