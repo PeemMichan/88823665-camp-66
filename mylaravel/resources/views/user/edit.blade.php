ff54472a7dc9b9e0922ff67926c0a816da86b7d6
peemMichan <naprarin2682@gmail.com>
2025-01-29 21:01:22 +0700

    laravel

--- mylaravel/resources/views/user/edit.blade.php
+++ mylaravel/resources/views/user/edit.blade.php
@@ -1,25 +1,29 @@
 @extends('layouts.default')
+
 @section('content')
 <div class="register-box">
     <div class="register-logo">
-      <a href="../index2.html"><b>Admin</b>LTE</a>
+      <a href=""><b>Edit</b>User</a>
     </div>
     <!-- /.register-logo -->
     <div class="card">
       <div class="card-body register-card-body">
-        <p class="register-box-msg">Register a new membership</p>
-        <form action="{{ url('/register')}}" method="post">
+        <p class="register-box-msg">Edit User</p>
+        <form action="{{ url('/user') }}" method="post">
             @csrf
+            @method('put')
+            <input type="hidden" name="id"
+            value="{{ $user->id}}" >
           <div class="input-group mb-3">
-            <input type="text" name = "name" class="form-control" placeholder="Full Name">
+            <input type="text" value="{{ $user->name}}" name="name" class="form-control" placeholder="Full Name">
             <div class="input-group-text"><span class="bi bi-person"></span></div>
           </div>
           <div class="input-group mb-3">
-            <input type="email" name = "email" class="form-control" placeholder="Email">
+            <input type="email" value="{{$user->email}}" name="email" class="form-control" placeholder="Email">
             <div class="input-group-text"><span class="bi bi-envelope"></span></div>
           </div>
           <div class="input-group mb-3">
-            <input type="password" name = "password" class="form-control" placeholder="Password">
+            <input type="password" name="password" class="form-control" placeholder="Password">
             <div class="input-group-text"><span class="bi bi-lock-fill"></span></div>
           </div>
           <!--begin::Row-->
@@ -42,19 +46,7 @@
           </div>
           <!--end::Row-->
         </form>
-        <div class="social-auth-links text-center mb-3 d-grid gap-2">
-          <p>- OR -</p>
-          <a href="#" class="btn btn-primary">
-            <i class="bi bi-facebook me-2"></i> Sign in using Facebook
-          </a>
-          <a href="#" class="btn btn-danger">
-            <i class="bi bi-google me-2"></i> Sign in using Google+
-          </a>
-        </div>
-        <!-- /.social-auth-links -->
-        <p class="mb-0">
-          <a href="login.html" class="text-center"> I already have a membership </a>
-        </p>
+
       </div>
       <!-- /.register-card-body -->
     </div>
